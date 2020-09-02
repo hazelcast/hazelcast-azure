f7a726005aaf77a9d4eff063d6552bae9d338f7f
Muhammet Yazici <muhammet.yazici@boun.edu.tr>
2020-09-02 12:34:25 +0300

    Fix indentation in verify_member_count.sh

--- .github/terraform/scripts/verify_member_count.sh
+++ .github/terraform/scripts/verify_member_count.sh
@@ -9,7 +9,8 @@ verify_hazelcast_cluster_size() {
     for i in `seq 1 5`; do
         local MEMBER_COUNT=$( curl http://127.0.0.1:5701/hazelcast/health/cluster-size )
 
-        if [ "$MEMBER_COUNT" == "$EXPECTED_SIZE" ] ; then            echo "Hazelcast cluster size equal to ${EXPECTED_SIZE}"
+        if [ "$MEMBER_COUNT" == "$EXPECTED_SIZE" ] ; then
+            echo "Hazelcast cluster size equal to ${EXPECTED_SIZE}"
             return 0
         else
             echo "Hazelcast cluster size NOT equal to ${EXPECTED_SIZE}!. Waiting.."
